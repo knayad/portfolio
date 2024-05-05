db065060a3cc7897bd11b0ea7751f1503f2dacf9
KNA <knayad@gmail.com>
2024-05-05 19:44:57 -0400

    reverting to 7ea2efa to try copyingfrom previous project for better layout.

--- src/App.js
+++ src/App.js
@@ -1,17 +1,23 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-
-import Home from "./pages/Home";
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">Nav?</header>
-      <BrowserRouter>
-        <Routes>
-          <Route index element={<Home />} />
-          <Route path="portfolio" element={<Home />} />
-        </Routes>
-      </BrowserRouter>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
