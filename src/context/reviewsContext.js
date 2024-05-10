0157ed8f826aad646d706a959b4bea5576cb8919
KNA <knayad@gmail.com>
2024-05-09 22:24:26 -0400

    create context, clean up, delete extra files

--- src/context/reviewsContext.js
+++ src/context/reviewsContext.js
@@ -1,1 +1,26 @@
-export const API_URL = "https://portfolio-api-op4o.onrender.com";
+import { createContext, useReducer } from "react";
+
+export const ReviewsContext = createContext();
+
+export const reviewsReducer = (state, action) => {
+  switch (action.type) {
+    case "SET_REVIEWS":
+      return {
+        reviews: action.payload,
+      };
+    case "CREATE_REVIEW":
+      return { reviews: [action.payload, ...state.reviews] };
+    default:
+      return state;
+  }
+};
+
+export const ReviewsContextProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(reviewsReducer, { reviews: null });
+
+  return (
+    <ReviewsContext.Provider value={{ state, dispatch }}>
+      {children}
+    </ReviewsContext.Provider>
+  );
+};
